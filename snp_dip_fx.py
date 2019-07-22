3286952dc63ef498b0fbe8249e3ab5708c12d789
Brandon Locke <blocke92@gmail.com>
2019-07-22 10:36:40 -0400

    Update snp_dip_fx.py

--- snp_dip_fx.py
+++ snp_dip_fx.py
@@ -109,7 +109,8 @@ def get_current_position(api, cur, conn):
 
         # deactivate all positions that do not exist anymore in alpaca
         if not df.empty:
-            sqlStrParam = "update snp_dip set active = false, exit_date = CURRENT_TIMESTAMP where strat_sk not in %s" 
+            sqlStrParam = "update snp_dip set active = false, exit_date = CURRENT_TIMESTAMP where strat_sk not in %s"
+            cur.mogrify(sqlStrParam, tuple([str(x) for x in df["strat_sk"]]))
             cur.execute(sqlStrParam, ([str(x) for x in df["strat_sk"]]))
             conn.commit()
         df["strat_sk"] = int(df["strat_sk"])
